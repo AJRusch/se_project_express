c8265681a17d226e7d3500fd2d656a1e2e012b1d
AJRusch <155108459+AJRusch@users.noreply.github.com>
2024-08-23 08:26:42 -0500

    Add files via upload

---
+++ controllers/clothingItems.js
@@ -0,0 +1,118 @@
+const ClothingItem = require("../models/clothingItem");
+const {
+  BAD_REQUEST,
+  DEFAULT,
+  NOT_FOUND,
+  UNAUTHORIZED,
+  FORBIDDEN,
+} = require("../utils/errors");
+const { SUCCESSFUL_REQUEST } = require("../utils/successStatus");
+
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(SUCCESSFUL_REQUEST).send(items))
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(DEFAULT)
+        .send({ message: "An error has occured on the server" });
+    });
+};
+
+const createItem = (req, res) => {
+  const { name, weather, imageUrl } = req.body;
+
+  ClothingItem.create({ name, weather, imageUrl, owner: req.user._id })
+    .then((item) => {
+      res.send({ data: item });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "ValidationError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+      }
+      return res
+        .status(DEFAULT)
+        .send({ message: "An error has occured on the server" });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+  const userId = req.user._id;
+
+  ClothingItem.findById(itemId)
+    .orFail()
+    .then((item) => {
+      if (item.owner.toString() !== userId) {
+        const error = new Error();
+        err.name = "Access Denied";
+        throw error;
+      }
+      return ClothingItem.findByIdAndDelete(itemId);
+    })
+    .then((item) => res.send({ item }))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "Access Denied") {
+        return res
+          .status(FORBIDDEN)
+          .send({ message: "You do not have access to delete this item" });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({ message: err.message });
+      }
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+      }
+      return res
+        .status(DEFAULT)
+        .send({ message: "An error has occured on the server" });
+    });
+};
+
+const likeItem = (req, res) => {
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((item) => res.send({ item }))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({ message: err.message });
+      }
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+      }
+      return res
+        .status(DEFAULT)
+        .send({ message: "An error has occured on the server" });
+    });
+};
+
+const dislikeItem = (req, res) => {
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((item) => res.send({ item }))
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(NOT_FOUND).send({ message: err.message });
+      }
+      if (err.name === "CastError") {
+        return res.status(BAD_REQUEST).send({ message: "Invalid data" });
+      }
+      return res
+        .status(DEFAULT)
+        .send({ message: "An error has occured on the server" });
+    });
+};
+
+module.exports = { getItems, createItem, deleteItem, likeItem, dislikeItem };
