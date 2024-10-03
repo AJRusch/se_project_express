f701b0ac28b3ee8e343a3caad101aa6ff90d0b76
AJRusch <155108459+AJRusch@users.noreply.github.com>
2024-10-03 11:30:50 -0500

    Delete middlewares directory

--- middlewares/error-handler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err);
-  const statusCode = err.statusCode || 500;
-  const message =
-    statusCode === 500 ? "An error occured on the server" : err.message;
-  res.status(statusCode).send({ message });
-};
-
-module.exports = errorHandler;
